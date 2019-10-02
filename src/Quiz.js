630005cbf42c5671139935fe72cb387d63ba1122
Brown <s.t.brown@accenture.com>
2019-10-02 11:24:23 -0500

    module 5

--- src/Quiz.js
+++ src/Quiz.js
@@ -12,12 +12,18 @@ class Quiz extends Component {
         }
     }
 
+    showNextQuestion () {
+        this.setState ((state) => {
+            return {quiz_position: state.quiz_position + 1}
+        })
+    }
+
     render () {
         const isQuizEnd = (this.state.quiz_position - 1 === quizData.quiz_questions.length)
         return (
             <div>
                 {isQuizEnd ? <QuizEnd /> :
-                <QuizQuestion quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
+                <QuizQuestion showNextQuestionHandler={this.showNextQuestion.bind(this)} quiz_question={quizData.quiz_questions[this.state.quiz_position - 1]} /> }
             </div>
         )
     }
